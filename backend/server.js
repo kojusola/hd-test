5092c62d77950ee899f76521af60a923075747eb
kojusola <adeola5678@gmail.com>
2024-11-14 06:05:53 +0000

    feat: added links and cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3002;
+var cors = require("cors");
 
 const data = {
-  navItems: [{
-    name: "Home",
-    url: "/"
-  },{
-    name: "New",
-    url: "/new"
-  },{
-    name: "Popular",
-    url: "/popular"
-  },{
-    name: "Trending",
-    url: "/trending"
-  },{
-    name: "Categories",
-    url: "/categories"
-  },],
+  navItems: [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "New",
+      url: "/new",
+    },
+    {
+      name: "Popular",
+      url: "/popular",
+    },
+    {
+      name: "Trending",
+      url: "/trending",
+    },
+    {
+      name: "Categories",
+      url: "/categories",
+    },
+  ],
   mainArticle: {
     title: "The Bright Future of Web 3.0?",
-    description: "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?",
-    imageUrl: "/path/to/main-image.jpg",
+    description:
+      "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?",
+    imageUrl: "/images/image-web-3-desktop.jpg",
     ctaButton: {
-        name: "READ MORE",
-        url: "/article?id=144"
-    }
+      name: "READ MORE",
+      url: "/article?id=144",
+    },
   },
   sidebar: {
     title: "New",
@@ -34,40 +42,49 @@ const data = {
       {
         title: "Hydrogen VS Electric Cars",
         description: "Will hydrogen-fueled cars ever catch up to EVs?",
-        url: "/article?id=124"
+        url: "/article?id=124",
       },
       {
         title: "The Downsides of AI Artistry",
-        description: "What are the possible adverse effects of on-demand AI image generation?",
-        url: "/article?id=132"
+        description:
+          "What are the possible adverse effects of on-demand AI image generation?",
+        url: "/article?id=132",
       },
       {
         title: "Is VC Funding Drying Up?",
-        description: "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
-        url: "/article?id=82"
-      }
-    ]
+        description:
+          "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+        url: "/article?id=82",
+      },
+    ],
   },
   articles: [
     {
       title: "Reviving Retro PCs",
       description: "What happens when old PCs are given modern upgrades?",
-      imageUrl: "/path/to/article1-image.jpg"
+      imageUrl: "/images/image-retro-pcs.jpg",
     },
     {
       title: "Top 10 Laptops of 2022",
       description: "Our best picks for various needs and budgets.",
-      imageUrl: "/path/to/article2-image.jpg"
+      imageUrl: "/images/image-top-laptops.jpg",
     },
     {
       title: "The Growth of Gaming",
       description: "How the pandemic has sparked fresh opportunities.",
-      imageUrl: "/path/to/article3-image.jpg"
-    }
-  ]
+      imageUrl: "/images/image-gaming-growth.jpg",
+    },
+  ],
 };
 
-app.get('/data', (req, res) => {
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.get("/data", (req, res) => {
   res.json(data);
 });
 
